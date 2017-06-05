4256fb8fab5019fac630dd12ca77587cf68ed7d3
Mara Timberlake and Steve Zaske <mara@>
2017-06-05 16:32:16 -0700

    Add loop logic to create a new array

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-$(function(){
-  alert("yup");
+//Business logic
+var suits = ["hearts","clubs","spades","diamonds"] //array for card suits
+var numbers = ["ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"] //array of #'s
+var newDeck = [];
+
+// steps through both arrays and create a new full deck
+suits.forEach(function(suit){
+  numbers.forEach(function(num){
+    newDeck.push(num+" of " + suit);
+  })
+
 });
+
+console.log(newDeck);
